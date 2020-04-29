f0253dfc27ef851f98a5ee802947e418f0731874
John Ross <jlross42@gmail.com>
2020-04-29 14:21:21 -0700

    fix metrics

--- main.py
+++ main.py
@@ -29,18 +29,19 @@ def main():
             songs[song_key] = None
 
     # download
+    songs_to_download = {song_key: song_name for song_key, song_name in songs.items() if song_name}
     base_download_url = 'https://beatsaver.com/api/download/key/'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0'}
     blacklist_chars = r'<>:"/\|?*'
     download_success_count = 0
     download_fail_count = 0
-    download_skip_count = 0
-    for n, (song_key, song_name) in enumerate(songs.items()):
+    download_skip_count = len(songs) - len(songs_to_download)
+    for n, (song_key, song_name) in enumerate(songs_to_download.items()):
         if not song_name:
             download_skip_count += 1
             continue
         download_url = f'{base_download_url}{song_key}'
-        print(f'{n}/{len(songs)}: downloading [{download_url}] - {song_name}')
+        print(f'{n}/{len(songs_to_download)}: downloading [{download_url}] - {song_name}')
         res = requests.get(download_url, allow_redirects=True, headers=headers)
         if not res.ok:
             print(res.status_code, res.content.decode())
